f78e8b587989940405d4e62afae20d664a70e1df
shreya shakya <shyaashakya23@example.com>
2018-04-03 12:14:47 +0545

    Add Email Notifier
    Email will be sent automatically to the reciever.

--- postbox.py
+++ postbox.py
@@ -89,3 +89,6 @@ for elem in table:
             pprint.pprint(data)
             notify('sender@gmail.com', 'senderpassword', 'reciever@gmail.com')
 
+#To automate the script everyday at 7am.
+#sudo crontab -e
+#0 7 * * * sudo -u <user> DISPLAY=:0 DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/1000/bus notify-send 'Hi' 'Check your mail.'| python /usr/bin/postbox.py
