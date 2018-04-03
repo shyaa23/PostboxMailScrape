c9f0c0594e3304fc35c8b10e69e4529233a644af
shreya shakya <shyaashakya23@example.com>
2018-04-03 13:02:10 +0545

    Add scheduler to automatically send mail at 7
    Instead of using crontab, used scheduler in script so that Mail is send at 7am automatically.

---
+++ postbox(notifyat7).py
@@ -0,0 +1,99 @@
+from lxml import html
+import requests
+import warnings
+import pprint
+import datetime
+from apscheduler.scheduler import Scheduler
+
+#Send Email Notification:
+def notify(sender, password, reciever):
+    import smtplib
+    from email.mime.multipart import MIMEMultipart
+    from email.mime.text import MIMEText
+
+    msg = MIMEMultipart()
+    msg['From'] = sender
+    msg['To'] = reciever
+    msg['Subject'] = 'Check Mail Notification'
+    message = 'Check Your Postbox.'
+    msg.attach(MIMEText(message))
+
+    mailserver = smtplib.SMTP('smtp.gmail.com',587)
+    # identify ourselves to smtp gmail client
+    mailserver.ehlo()
+    # secure our email with tls encryption
+    mailserver.starttls()
+    # re-identify ourselves as an encrypted connection
+    mailserver.ehlo()
+    mailserver.login(sender, password)
+    mailserver.sendmail(sender,reciever,msg.as_string())
+    mailserver.quit()
+
+# Start the scheduler
+sched = Scheduler()
+sched.daemonic = False
+sched.start()
+
+def job_function():
+    warnings.filterwarnings("ignore")
+
+    time = datetime.datetime.now()
+
+    USERNAME = "****"
+    PASSWORD = "*******"
+
+    login_url = 'https://onlinepostbox.gpo.gov.np/'
+    url = "https://onlinepostbox.gpo.gov.np/PBOnlineMail/ListOnlineMail"
+    session_requests = requests.session()
+
+    # Get login csrf token
+    response = session_requests.get(login_url, verify=False)
+    tree = html.fromstring(response.text)
+    csrf_token = tree.xpath("//input[@name='__RequestVerificationToken']/@value")
+
+    # Create payload
+    payload = {
+        "username": USERNAME,
+        "password": PASSWORD,
+        "__RequestVerificationToken": csrf_token
+    }
+
+    # Perform login
+    result = session_requests.post(login_url, data=payload, headers=dict(referer=login_url))
+
+    # Scrape url
+    postbox = session_requests.get(url, headers=dict(referer=url))
+    tree = html.fromstring(postbox.content)
+
+    table = tree.xpath('//*[@id="body"]/div[2]/div/section/div[2]/div/table')
+    for elem in table:
+        thead = elem.xpath('//*[@id="body"]/div[2]/div/section/div[2]/div/table/thead')
+        tbody = elem.xpath('//*[@id="body"]/div[2]/div/section/div[2]/div/table/tbody')
+
+        for title, value in zip(thead, tbody):
+            thead_th = title.xpath('//*[@id="body"]/div[2]/div/section/div[2]/div/table/thead/tr/th/text()')
+            thead_th = [word.strip() for word in thead_th]
+            rows = title.xpath("//*[@id='body']/div[2]/div/section/div[2]/div/table/tbody/tr")
+            if rows:
+                for i, item in enumerate(rows):
+                    tbody_td = item.xpath(
+                        '//*[@id="body"]/div[2]/div/section/div[2]/div/table/tbody/tr' + str([i + 1]) + '/td/text()')
+            else:
+                tbody_td = value.xpath('//*[@id="body"]/div[2]/div/section/div[2]/div/table/tbody/tr/td/text()')
+                tbody_td = [word.strip() for word in tbody_td]
+
+            if tbody_td == []:
+                print("Empty Postbox........No Mail List")
+                data = dict(map(lambda e: (e, ' '), thead_th))
+                data['Mail Checked At'] = str(time)
+                pprint.pprint(data)
+                print("\n")
+                notify('sender@gmail.com', 'senderpassword', 'reciever@gmail.com')
+
+            else:
+                data = dict(zip(thead_th, tbody_td))
+                data['Mail Checked At'] = str(time)
+                pprint.pprint(data)
+                notify('sender@gmail.com', 'senderpassword', 'reciever@gmail.com')
+# Schedules job_function to be run once each minute
+sched.add_cron_job(job_function, hour='7', minute='0')
